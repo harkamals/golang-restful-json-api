e74576051dd3bf2bd4e0bde8615c488ddcc6c968
HK <harkamals@gmail.com>
2017-09-12 17:20:43 +0800

    v2: Added Testing

--- v2/orders.go
+++ v2/orders.go
@@ -24,10 +24,7 @@ var orderId int
 var orders Orders
 
 func orders_list(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Println("Orders List")
 	json_encoder(w, http.StatusOK, orders)
-
 }
 
 func init() {
