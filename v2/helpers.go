bfe7b1b0a8060f96884ada417a9e69f55f36c840
HK <harkamals@gmail.com>
2017-09-14 03:20:30 +0800

    v2: Added Database backend

--- v2/helpers.go
+++ v2/helpers.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"net/http"
 	"encoding/json"
+	"log"
+	"net/http"
+	"time"
 )
 
 func json_encoder(w http.ResponseWriter, statusCode int, input interface{}) {
@@ -23,3 +25,18 @@ func respondWithError(w http.ResponseWriter, code int, message string) {
 	json_encoder(w, code, jsonErr{Code: code, Text: message})
 }
 
+func Logger(inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		inner.ServeHTTP(w, r)
+
+		log.Printf(
+			"%s %-s %s %s",
+			r.Method,
+			r.RequestURI,
+			name,
+			time.Since(start),
+		)
+	})
+}
